57abef2a9842414b6a738b0b177cf8d49ee9605d
OProCode <proslmeyr@icloud.com>
2025-07-22 13:37:52 +0800

    Changed labels list to dictionary, loop now creates different widgets corresponding to the label.

--- main.py
+++ main.py
@@ -5,34 +5,28 @@ app = tk.Tk()
 app.title("Quote Estimate")
 
 app.geometry("800x200")
-
-entries = []
-labels = ["Base Rate", "Length", "Implementation"]
-buttons = []
+labels = [{"type": "entry", "name": "Base Rate"}, {"type": "entry", "name": "Length"}, {"type": "combo", "name": "Complexity"}, {"type": "entry", "name": "Variations"}, {"type": "combo", "name": "Implementation"}, {"type": "entry", "name": "Buffer"}, {"type": "label", "name": "Total"}]
 
 frame_column = ttk.Frame(app)
-frame_column.columnconfigure(0, weight=1)
-frame_column.columnconfigure(1, weight=1)
-frame_column.columnconfigure(2, weight=1)
 
-frame_row = ttk.Frame(app)
-frame_row.rowconfigure(0, weight=1)
-frame_row.rowconfigure(1, weight=1)
-
-# TODO: Align Label above each Entry widget
+for i, label in enumerate(labels):
+    frame_column.columnconfigure(i, weight=1)
 
 for index, label in enumerate(labels):
-    # tk.Label requires a parent widget (app in this case) and a text argument, with optinal font parameters
-    label = tk.Label(frame_column, text=label)
-    label.grid(row=0, column=index, padx=5, pady=5, stick="we")
+    label_iteration = tk.Label(frame_column, text=label["name"])
+    label_iteration.grid(row=0, column=index, padx=5, pady=5)
    
-    entry = tk.Entry(frame_column)
-    #  Pack prints the widget in the parent widget (app) and allows for padding and side alignment
-    entry.grid(row=1, column=index, padx=5, pady=5, sticky="we")
+    if label["type"] == "entry":
+        entry = tk.Entry(frame_column)
+        entry.grid(row=1, column=index, padx=5, pady=5)
+    if label["type"] == "combo":
+        combo = ttk.Combobox(frame_column)
+        combo.grid(row=1, column=index, padx=5, pady=5)
+    if label["type"] == "label":
+        text = tk.Label(frame_column, text="Calculate Total Hours")
+        text.grid(row=1, column=index, padx=5, pady=5)
 
-    entries.append(entry)
-
-frame_column.pack(padx=10, pady=10, fill=tk.X)
+frame_column.pack(padx=5, pady=5, fill=tk.X)
 
 # button = tk.Button(app, text="Calculate", command=lambda: print("Calculating..."))
 # button.pack(padx=5, pady=5, side=tk.LEFT)
